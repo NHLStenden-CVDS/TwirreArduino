489e3ed26a2df1338fd2f160c8341eb2864091e7
Chris <christiaan.elsinga@nhl.nl>
2015-11-06 13:42:12 +0100

    Make compatible with Twirreshield V2

--- uav_interface/Vsense.h
+++ uav_interface/Vsense.h
@@ -3,18 +3,39 @@
 
 #include "Device.h"
 
+#define LIPOP4 A0
+#define LIPOP3 A1
+#define LIPOP2 A2
+#define LIPOP1 A3
+
+//TODO: CALIBRATE REF AND RATIO VALUES
+#define REFVOLTAGE 3.3
+
+#define CELL1RATIO 7.825
+#define CELL2RATIO 7.825
+#define CELL3RATIO 7.80
+#define CELL4RATIO 7.80
+
+#define RESOLUTION 4096.0
+
 class VSense: public Device
 {
   public:
-    VSense(int pin, double Vmin, double Vmax, char* name);
+    VSense(char* name);
     virtual void OnRequest();
     
   private:
-    int _pin;
     double _voltage;
-    double _vmin;
-    double _vmax;
-    double _dV;
+
+    double _raw1;
+    double _raw2;
+    double _raw3;
+    double _raw4;
+    
+    double _vcell1;
+    double _vcell2;
+    double _vcell3;
+    double _vcell4;
 };
 
 #endif
