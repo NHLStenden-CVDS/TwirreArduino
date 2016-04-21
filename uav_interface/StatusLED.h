2055674328f83edef183fcc03a45f59741199630
Chris Elsinga <christiaan.elsinga@nhl.nl>
2016-04-21 11:51:21 +0200

    Fix Statusled order, add method for direct control

--- uav_interface/StatusLED.h
+++ uav_interface/StatusLED.h
@@ -6,14 +6,15 @@
 #include <inttypes.h>
 
 #define LEDR 48
-#define LEDG 47
-#define LEDB 46
+#define LEDG 46
+#define LEDB 47
 
 class StatusLED : public Device
 {
   public:
     StatusLED(const char* name);
     void ValuesChanged();
+    void SetValue(int8_t _red,int8_t green, int8_t blue);
   private:
     int8_t _red = 0, _green = 0, _blue = 0;
 };
